59e8fae92aa1f51e9d440e9314695a339ef1d501
Nicolò Santamaria <nicolo.santamaria@protonmail.com>
2023-06-11 16:25:37 +0200

    Fix crash, handle states and set commands.

--- main.go
+++ main.go
@@ -1,26 +1,37 @@
 package main
 
 import (
-	"fmt"
+	_ "embed"
 	"log"
+	"strings"
 	"time"
 
 	"github.com/NicoNex/echotron/v3"
-	cohere "github.com/cohere-ai/cohere-go"
+	"github.com/cohere-ai/cohere-go"
 )
+
+// Recursive type definition of the bot state function.
+type stateFn func(*echotron.Update) stateFn
 
 // Struct useful for managing internal states in your bot, but it could be of
 // any type such as `type bot int64` if you only need to store the chatID.
 type bot struct {
-	chatID       int64
+	chatID int64
 	echotron.API
 	cohereClient *cohere.Client
-	promptInput  bool
+	state        stateFn
 }
 
-const (
-	token        = "TOKEN KEY"
-	cohereAPIKey = "API KEY"
+var (
+	//go:embed tgtoken
+	token string
+	//go:embed chtoken
+	cohereAPIKey string
+
+	commands = []echotron.BotCommand{
+		{Command: "/start", Description: "Activate the bot."},
+		{Command: "/generate", Description: "Generate an answer."},
+	}
 )
 
 // This function needs to be of type 'echotron.NewBotFn' and is called by
@@ -31,36 +42,45 @@ const (
 func newBot(chatID int64) echotron.Bot {
 	cohereClient, err := cohere.CreateClient(cohereAPIKey)
 	if err != nil {
-		log.Println(err)
-		return nil
+		log.Fatalln(err)
 	}
 
-	return &bot{
+	b := &bot{
 		chatID:       chatID,
 		API:          echotron.NewAPI(token),
 		cohereClient: cohereClient,
-		promptInput:  false,
 	}
+	b.state = b.handleMessage
+	return b
+}
+
+func (b *bot) handlePrompt(update *echotron.Update) stateFn {
+	response, err := b.generateText(message(update))
+	if err != nil {
+		log.Println("handlePrompt", err)
+		b.SendMessage("An error occurred!", b.chatID, nil)
+		return b.handleMessage
+	}
+	b.SendMessage(response, b.chatID, nil)
+	return b.handleMessage
+}
+
+func (b *bot) handleMessage(update *echotron.Update) stateFn {
+	switch m := message(update); {
+
+	case strings.HasPrefix(m, "/start"):
+		b.SendMessage("Hello world", b.chatID, nil)
+
+	case strings.HasPrefix(m, "/generate"):
+		b.SendMessage("Please enter a prompt:", b.chatID, nil)
+		return b.handlePrompt
+	}
+	return b.handleMessage
 }
 
 // This method is needed to implement the echotron.Bot interface.
 func (b *bot) Update(update *echotron.Update) {
-	if update.Message.Text == "/start" {
-		b.SendMessage("Hello world", b.chatID, nil)
-	} else if update.Message.Text == "/generate" {
-		b.SendMessage("Please enter a prompt:", b.chatID, nil)
-		b.promptInput = true
-	} else if b.promptInput {
-		prompt := update.Message.Text
-		response, err := b.generateText(prompt)
-		if err != nil {
-			b.SendMessage(fmt.Sprintf("Error: %s", err.Error()), b.chatID, nil)
-			return
-		}
-
-		b.SendMessage(response, b.chatID, nil)
-		b.promptInput = false
-	}
+	b.state = b.state(update)
 }
 
 // Generate text using the Cohere API
@@ -83,7 +103,21 @@ func (b *bot) generateText(prompt string) (string, error) {
 	return response.Generations[0].Text, nil
 }
 
+// Returns the message from the given update.
+func message(u *echotron.Update) string {
+	if u.Message != nil {
+		return u.Message.Text
+	} else if u.EditedMessage != nil {
+		return u.EditedMessage.Text
+	} else if u.CallbackQuery != nil {
+		return u.CallbackQuery.Data
+	}
+	return ""
+}
+
 func main() {
+	echotron.NewAPI(token).SetMyCommands(nil, commands...)
+
 	// This is the entry point of echotron library.
 	dsp := echotron.NewDispatcher(token, newBot)
 	for {
