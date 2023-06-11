a6416050b93c36f0aca82114512f24816e530874
Gigi Kenneth <gigikenneth7@gmail.com>
2023-06-11 05:24:14 +0000

    no error messages

---
+++ go.mod
@@ -0,0 +1,14 @@
+module textjen
+
+go 1.20
+
+require (
+	github.com/NicoNex/echotron/v3 v3.25.1
+	github.com/cohere-ai/cohere-go v1.2.2
+)
+
+require (
+	github.com/cohere-ai/tokenizer v1.1.1 // indirect
+	github.com/dlclark/regexp2 v1.4.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/NicoNex/echotron/v3"
+	cohere "github.com/cohere-ai/cohere-go"
+)
+
+// Struct useful for managing internal states in your bot, but it could be of
+// any type such as `type bot int64` if you only need to store the chatID.
+type bot struct {
+	chatID       int64
+	echotron.API
+	cohereClient *cohere.Client
+}
+
+const (
+	token        = "KEY"
+	cohereAPIKey = "API KEY"
+)
+
+// This function needs to be of type 'echotron.NewBotFn' and is called by
+// the echotron dispatcher upon any new message from a chatID that has never
+// interacted with the bot before.
+// This means that echotron keeps one instance of the echotron.Bot implementation
+// for each chat where the bot is used.
+func newBot(chatID int64) echotron.Bot {
+	cohereClient, err := cohere.CreateClient(cohereAPIKey)
+	if err != nil {
+		log.Println(err)
+		return nil
+	}
+
+	return &bot{
+		chatID:       chatID,
+		API:          echotron.NewAPI(token),
+		cohereClient: cohereClient,
+	}
+}
+
+// This method is needed to implement the echotron.Bot interface.
+func (b *bot) Update(update *echotron.Update) {
+	if update.Message.Text == "/start" {
+		b.SendMessage("Hello world", b.chatID, nil)
+	} else if update.Message.Text == "/generate" {
+		response, err := b.generateText("give me an idea for a book")
+		if err != nil {
+			b.SendMessage(fmt.Sprintf("Error: %s", err.Error()), b.chatID, nil)
+			return
+		}
+
+		b.SendMessage(response, b.chatID, nil)
+	}
+}
+
+// Generate text using the Cohere API
+func (b *bot) generateText(prompt string) (string, error) {
+	options := cohere.GenerateOptions{
+		Model:             "command",
+		Prompt:            prompt,
+		MaxTokens:         300,
+		Temperature:       0.9,
+		K:                 0,
+		StopSequences:     []string{},
+		ReturnLikelihoods: "NONE",
+	}
+
+	response, err := b.cohereClient.Generate(options)
+	if err != nil {
+		return "", err
+	}
+
+	return response.Generations[0].Text, nil
+}
+
+func main() {
+	// This is the entry point of echotron library.
+	dsp := echotron.NewDispatcher(token, newBot)
+	for {
+		err := dsp.Poll()
+		if err != nil {
+			log.Println("Error polling updates:", err)
+		}
+
+		// In case of connection issues, wait 5 seconds before trying to reconnect.
+		time.Sleep(5 * time.Second)
+	}
+}
